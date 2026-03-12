062d7eb4a815d7ffdbe3b199ede5a6c820d959d3
Diego Alarcón <tu_correo_de_github@example.com>
2026-03-12 15:24:07 -0300

    update v2_cache

--- app/assets/js/v2_cache.js
+++ app/assets/js/v2_cache.js
@@ -1,6 +1,6 @@
 (function(){
   'use strict';
-  window.SW_VERSION = window.SW_VERSION || 'v6.0.0';
+  window.SW_VERSION = window.SW_VERSION || 'v6.0.1';
   const DB_NAME    = 'visibility2-v2';
   const DB_VERSION = 9;
   
